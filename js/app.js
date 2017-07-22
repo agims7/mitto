46cef7010fd041fb423f1c978fa917c6c7d3bcb5
Michal Dydo <michal1.dydo@gmail.com>
2017-07-22 22:49:53 +0200

    finalna wersja do testow

--- js/app.js
+++ js/app.js
@@ -1,13 +1,9 @@
-console.log('cos')
-
 $(document).ready(function() {
     $(window).scroll(function() {
         if ($(document).scrollTop() > (window.innerHeight - 84)) {
             $(".navbar").css("background-color", "#39c0a8");
-            console.log('cos1')
         } else {
             $(".navbar").css("background-color", "transparent");
-            console.log('cos2')
         }
     });
 
@@ -33,4 +29,14 @@ $(document).ready(function() {
             });
         } // End if
     });
+
+    $(".video-link").on('click', function(event) {
+        $(".youtube-container").css("display", "block");
+    });
+
+    $(".youtube-close-icon ").on('click', function(event) {
+        $(".youtube-container").css("display", "none");
+    });
+
+
 });
---
+++ mail.php
@@ -0,0 +1,12 @@
+<?php 
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $tel = $_POST['tel'];
+    $formcontent="Adres email: $email \n\nTelefon: $tel \n\nTresc wiadomosci: $message";
+    $recipient = "michal.dydo@mittoplus.pl";
+    $subject = "Formularz kontaktowy ze strony Mitto+";
+    $mailheader = "Od: $email \r\n";
+    mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+    echo "<script type='text/javascript'>alert('Formularz zostal wysłany!')</script>";
+    print "<meta http-equiv=\"refresh\" content=\"0;URL=index.html\">";
+?>
