9153be761cc9297c6c2ba28791026a40578c597a
michal.dydo <michal.dydo@rnt.com.pl>
2017-07-24 08:42:24 +0200

    poprawki video

--- js/app.js
+++ js/app.js
@@ -7,36 +7,35 @@ $(document).ready(function() {
         }
     });
 
-    // Add smooth scrolling to all links
+    //Smooth scrolling
     $(".pointer").on('click', function(event) {
-
-        // Make sure this.hash has a value before overriding default behavior
         if (this.hash !== "") {
-            // Prevent default anchor click behavior
             event.preventDefault();
-
-            // Store hash
             var hash = this.hash;
-
-            // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
             $('html, body').animate({
                 scrollTop: $(hash).offset().top - 84
             }, 800, function() {
-
-                // Add hash (#) to URL when done scrolling (default click behavior)
                 window.location.hash = hash;
             });
-        } // End if
+        }
     });
 
+    //Video section
     $(".video-link").on('click', function(event) {
         $(".youtube-container").css("display", "block");
+        playVideo();
     });
 
-    $(".youtube-close-icon ").on('click', function(event) {
+    $(".youtube-container").on('click', function(event) {
+        stopVideo();
         $(".youtube-container").css("display", "none");
     });
 
+    $(document).keyup(function(e) {
+        if (e.keyCode == 27) {
+            stopVideo();
+            $(".youtube-container").css("display", "none");
+        }
+    });
 
 });
