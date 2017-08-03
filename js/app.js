87aa06583268fee51d1d7668707a00940d163805
michal.dydo <michal.dydo@rnt.com.pl>
2017-08-03 09:09:03 +0200

    ADD dodanie tabsow

--- js/app.js
+++ js/app.js
@@ -1,24 +1,15 @@
 $(document).ready(function() {
+
+    /////////////// Navbar fixed / background ///////////////
     $(window).scroll(function() {
-        if ($(document).scrollTop() > (window.innerHeight - 84)) {
+        if ($(document).scrollTop() > (window.inneropacity - 84)) {
             $(".navbar").css("background-color", "#39c0a8");
         } else {
             $(".navbar").css("background-color", "transparent");
         }
     });
 
-    // //Smooth scrolling
-    // $(".pointer").on('click', function(event) {
-    //     if (this.hash !== "") {
-    //         event.preventDefault();
-    //         var hash = this.hash;
-    //         $('html, body').animate({
-    //             scrollTop: $(hash).offset().top - 84
-    //         }, 800, function() {
-    //             window.location.hash = hash;
-    //         });
-    //     }
-    // });
+    /////////////// Smooth scrolling ///////////////
     $(document).on('click', 'a', function(event) {
         event.preventDefault();
 
@@ -27,7 +18,7 @@ $(document).ready(function() {
         }, 800);
     });
 
-    //Video section
+    /////////////// Video section ///////////////
     $(".video-link").on('click', function(event) {
         $(".youtube-container").css("display", "block");
         playVideo();
@@ -44,5 +35,97 @@ $(document).ready(function() {
             $(".youtube-container").css("display", "none");
         }
     });
+
+    /////////////// TABS ///////////////
+    $(".paragraph-first").on('click', function(event) {
+        // $(".paragraph-first-second").css("display", "block");
+        // $(".paragraph-second-second").css("display", "none");
+        // $(".paragraph-third-second").css("display", "none");
+        // $(".paragraph-fourth-second").css("display", "none");
+        $(".paragraph-first-second").css("opacity", "1");
+        $(".paragraph-second-second").css("opacity", "0");
+        $(".paragraph-third-second").css("opacity", "0");
+        $(".paragraph-fourth-second").css("opacity", "0");
+        $(".paragraph-first-second").css("height", "78px");
+        $(".paragraph-second-second").css("height", "0");
+        $(".paragraph-third-second").css("height", "0");
+        $(".paragraph-fourth-second").css("height", "0");
+    });
+
+    $(".paragraph-second").on('click', function(event) {
+        // $(".paragraph-first-second").css("display", "none");
+        // $(".paragraph-second-second").css("display", "block");
+        // $(".paragraph-third-second").css("display", "none");
+        // $(".paragraph-fourth-second").css("display", "none");
+        $(".paragraph-first-second").css("opacity", "0");
+        $(".paragraph-second-second").css("opacity", "1");
+        $(".paragraph-third-second").css("opacity", "0");
+        $(".paragraph-fourth-second").css("opacity", "0");
+        $(".paragraph-first-second").css("height", "0");
+        $(".paragraph-second-second").css("height", "78px");
+        $(".paragraph-third-second").css("height", "0");
+        $(".paragraph-fourth-second").css("height", "0");
+    });
+
+    $(".paragraph-third").on('click', function(event) {
+        // $(".paragraph-first-second").css("display", "none");
+        // $(".paragraph-second-second").css("display", "none");
+        // $(".paragraph-third-second").css("display", "block");
+        // $(".paragraph-fourth-second").css("display", "none");
+        $(".paragraph-first-second").css("opacity", "0");
+        $(".paragraph-second-second").css("opacity", "0");
+        $(".paragraph-third-second").css("opacity", "1");
+        $(".paragraph-fourth-second").css("opacity", "0");
+        $(".paragraph-first-second").css("height", "0");
+        $(".paragraph-second-second").css("height", "0");
+        $(".paragraph-third-second").css("height", "78px");
+        $(".paragraph-fourth-second").css("height", "0");
+    });
+
+    $(".paragraph-fourth").on('click', function(event) {
+        // $(".paragraph-first-second").css("display", "none");
+        // $(".paragraph-second-second").css("display", "none");
+        // $(".paragraph-third-second").css("display", "none");
+        // $(".paragraph-fourth-second").css("display", "block");
+        $(".paragraph-first-second").css("opacity", "0");
+        $(".paragraph-second-second").css("opacity", "0");
+        $(".paragraph-third-second").css("opacity", "0");
+        $(".paragraph-fourth-second").css("opacity", "1");
+        $(".paragraph-first-second").css("height", "0");
+        $(".paragraph-second-second").css("height", "0");
+        $(".paragraph-third-second").css("height", "0");
+        $(".paragraph-fourth-second").css("height", "78px");
+    });
+
+    ///////////////////////////
+
+    // $(".paragraph-first").on('click', function(event) {
+    //     $(".paragraph-first-second").css("height", "78px");
+    //     $(".paragraph-second-second").css("height", "0");
+    //     $(".paragraph-third-second").css("height", "0");
+    //     $(".paragraph-fourth-second").css("height", "0");
+    // });
+
+    // $(".paragraph-second").on('click', function(event) {
+    // $(".paragraph-first-second").css("height", "0");
+    // $(".paragraph-second-second").css("height", "78px");
+    // $(".paragraph-third-second").css("height", "0");
+    // $(".paragraph-fourth-second").css("height", "0");
+    // });
+
+    // $(".paragraph-third").on('click', function(event) {
+    //     $(".paragraph-first-second").css("height", "0");
+    //     $(".paragraph-second-second").css("height", "0");
+    //     $(".paragraph-third-second").css("height", "78px");
+    //     $(".paragraph-fourth-second").css("height", "0");
+    // });
+
+    // $(".paragraph-fourth").on('click', function(event) {
+    //     $(".paragraph-first-second").css("height", "0");
+    //     $(".paragraph-second-second").css("height", "0");
+    //     $(".paragraph-third-second").css("height", "0");
+    //     $(".paragraph-fourth-second").css("height", "78px");
+    // });
+
 
 });
