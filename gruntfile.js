96e3587260c240d33d264e117db79ab8884cfd2b
michal.dydo <michal.dydo@rnt.com.pl>
2017-08-04 09:01:42 +0200

    Dodanie grunt funkcji

--- gruntfile.js
+++ gruntfile.js
@@ -1,30 +1,58 @@
-module.exports = function (grunt) {
+module.exports = function(grunt) {
 
-  grunt.initConfig({
-    sass: {
-      options: {
-        sourceMap: true
-      },
-      dist: {
-        files: {
-          'css/style.css': 'css/style.scss'
-        }
-      }
-    },
-    watch: {
-      scripts: {
-        files: ['css/*.scss'],
-        tasks: ['sass'],
-        options: {
-          spawn: false,
+    grunt.initConfig({
+        sass: {
+            options: {
+                sourceMap: true
+            },
+            dist: {
+                files: {
+                    'css/style.css': 'css/style.scss'
+                }
+            }
         },
-      }
-    }
-  });
 
-  grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+        watch: {
+            scripts: {
+                files: ['css/*.scss'],
+                tasks: ['sass'],
+                options: {
+                    spawn: false,
+                },
+            }
+        },
 
-  // Default task(s)
-  grunt.registerTask('default', ['watch']);
+        cssmin: {
+            options: {
+                mergeIntoShorthands: false,
+                roundingPrecision: -1
+            },
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: 'css',
+                    src: ['*.css', '!*.min.css'],
+                    dest: 'css',
+                    ext: '.min.css'
+                }]
+            }
+        },
+
+        uglify: {
+            my_target: {
+                files: {
+                    'js/app.min.js': 'js/app.js'
+                }
+            }
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Default task(s)
+    grunt.registerTask('default', ['watch']);
 };
