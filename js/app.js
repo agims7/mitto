42ff1823709857459d7e03e07e65e8ffd1d62c28
Michal Dydo <michal1.dydo@gmail.com>
2017-09-29 14:13:40 +0200

    Add dodanie cennika i poprawa grida

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     if (window.innerWidth <= 768) {
         $('.options').slick({
@@ -15,7 +15,7 @@ $(document).ready(function() {
         });
     }
 
-    window.onresize = function() {
+    window.onresize = function () {
         if (window.innerWidth <= 768) {
             $('.options').slick({
                 dots: true,
@@ -35,7 +35,7 @@ $(document).ready(function() {
     };
 
     /////////////// Navbar fixed / background ///////////////
-    $(window).scroll(function() {
+    $(window).scroll(function () {
         if ($(document).scrollTop() > (window.innerHeight - 84)) {
             $(".navbar").css("background-color", "#39c0a8");
         } else {
@@ -44,42 +44,92 @@ $(document).ready(function() {
     });
 
     /////////////// Smooth scrolling ///////////////
-    $(document).on('click', 'a', function() {
+    $(document).on('click', 'a', function () {
         $('html, body').animate({
             scrollTop: $($.attr(this, 'href')).offset().top - 84
         }, 800);
     });
 
     /////////////// Close cookie ///////////////
-    $(".cookies-button").on('click', function() {
+    $(".cookies-button").on('click', function () {
         $(".cookies").css("display", "none");
         $(".navbar").css("top", "0");
     });
 
     /////////////// Video section ///////////////
 
-    $(".video-link").on('click', function(event) {
+    $(".video-link").on('click', function (event) {
         $(".youtube-container").css("display", "block");
         playVideo();
     });
 
-    $(".youtube-container").on('click', function(event) {
+    $(".youtube-container").on('click', function (event) {
         stopVideo();
         $(".youtube-container").css("display", "none");
     });
 
-    $(document).keyup(function(e) {
+    $(document).keyup(function (e) {
         if (e.keyCode == 27) {
             stopVideo();
             $(".youtube-container").css("display", "none");
         }
     });
 
+    /////////////// MODAL section ///////////////
+
+    $(".brown-price").on('click', function (event) {
+        $(".modal").css("display", "block");
+        var elem = $(".brown-element");
+        setTimeout(function () { scrollTo(elem); }, 100);
+    });
+
+    $(".silver-price").on('click', function (event) {
+        $(".modal").css("display", "block");
+        var elem = $(".silver-element");
+        setTimeout(function () { scrollTo(elem); }, 100);
+    });
+
+    $(".gold-price").on('click', function (event) {
+        $(".modal").css("display", "block");
+        var elem = $(".gold-element");
+        setTimeout(function () { scrollTo(elem); }, 100);
+    });
+
+    $(".platinum-price").on('click', function (event) {
+        $(".modal").css("display", "block");
+        var elem = $(".platinum-element");
+        setTimeout(function () { scrollTo(elem); }, 100);
+    });
+
+    function scrollTo(elem) {
+        if (elem.length) {
+            $(".modal").animate({ scrollTop: elem["0"].offsetTop }, { duration: 'medium', easing: 'swing' });
+        }
+    }
+
+    $(".header-close-icon").on('click', function (event) {
+        $(".modal").css("display", "none");
+    });
+
+    $(document).keyup(function (e) {
+        if (e.keyCode == 27) {
+            $(".modal").css("display", "none");
+        }
+    });
+
+    window.onclick = function(event) {
+        var modal = $(".modal");
+        if (event.target.id == modal["0"].id) {
+            $(".modal").css("display", "none");
+        }
+    }
+
+
     /////////////// TABS ///////////////
 
     var intervalFunctions = [second, third, fourth, first];
     var intervalIndex = 0;
-    window.setInterval(function() {
+    window.setInterval(function () {
         intervalFunctions[intervalIndex++ % intervalFunctions.length]();
     }, 5500);
 
